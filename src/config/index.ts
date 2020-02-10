cb1dac30f0b10e3d5740556c049588d00bb5b310
Bogdan Shevchenko <bohdan.shevchenko1@nure.ua>
2020-02-10 06:51:32 +0200

    [Not working] Fix loading error

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,12 +2,13 @@ import * as nconf from 'nconf';
 import * as path from 'path';
 import { promises as fs } from 'fs';
 import { path as appRootPath } from 'app-root-path';
-import { Argv } from "yargs";
-import { IFullAppConfig } from "./types";
-import iterate from "iterare";
-import { Nullable } from "../@types";
-import {commonCamelCase, objectKeys} from "../utils/common";
-import {file} from "nconf";
+import { Argv } from 'yargs';
+import { IFullAppConfig } from './types';
+import iterate from 'iterare';
+import { Nullable } from '../@types';
+import { commonCamelCase, objectKeys } from '../utils/common';
+import * as YAML from 'yaml';
+import * as TOML from '@iarna/toml';
 
 export const appConfigPrefix = nameof<IFullAppConfig['ncgc']>();
 export const environmentVariableDepthSeparator = '__';
@@ -16,21 +17,21 @@ export const configDirectoryKey = nameof<IFullAppConfig['ncgc']['configDir']>();
 export const defaultConfigDirectory = path.join(appRootPath, 'config');
 
 export function tryGetConfigDirFromEnv() {
-  return process.env[transformAppEnvConfigKey(
-    iterate(objectKeys(process.env)).filter(key => isAppEnvConfigKey(key)).take(1).toArray()[0]
+  return process.env[transformAppEnvConfigKey(iterate(objectKeys(process.env))
+      .filter(key => isAppEnvConfigKey(key)).take(1).toArray()[0],
   )];
 }
 
 export enum Environment {
   Development = 'development',
   Production = 'production',
-  Test = 'test'
+  Test = 'test',
 }
 
 export const environment: Environment | string = process.env.NODE_ENV?.trim()
   .toLowerCase() || Environment.Development;
 
-let config: Nullable<IFullAppConfig> = null;
+const config: Nullable<IFullAppConfig> = null;
 let configDirectory: Nullable<string> = null;
 
 export function isConfigInitialized() {
@@ -57,10 +58,10 @@ export async function initializeConfig<T extends IFullAppConfig>(argv: Argv<T>) 
   }
   const configDirectoryOverrides = [
     tryGetConfigDirFromEnv(),
-    argv.argv.ncgc.configDir
+    argv.argv.ncgc.configDir,
   ].filter(v => typeof v === 'string');
   const configDir = normalizeConfigDirPath(
-    configDirectoryOverrides[0] || defaultConfigDirectory
+    configDirectoryOverrides[0] || defaultConfigDirectory,
   );
 
   const stat = await fs.stat(configDir);
@@ -91,7 +92,9 @@ export async function initializeConfig<T extends IFullAppConfig>(argv: Argv<T>) 
 
 function normalizeConfigDirPath(possiblePath: string) {
   const configDirectory = path.normalize(possiblePath.trim());
-  return path.isAbsolute(configDirectory) ? configDirectory : path.resolve(configDirectory);
+  return path.isAbsolute(configDirectory)
+    ? configDirectory
+    : path.resolve(configDirectory);
 }
 
 function initializeNconfSync<T extends IFullAppConfig>(argv: Argv<T>) {
@@ -110,21 +113,29 @@ function initializeNconfSync<T extends IFullAppConfig>(argv: Argv<T>) {
     'default',
     environment,
     'local',
-    `local-${environment}`
+    `local-${environment}`,
   ].flatMap(
-    b => ['.json', '.yaml', '.yml', '.toml'].map(ext => `${b}${ext}`)
+    b => [
+      ['.json', JSON],
+      ['.yaml', YAML],
+      ['.yml', YAML],
+      ['.toml', TOML],
+    ].map(ext => `${b}${ext}`),
   ).map(f => path.join(getConfigDirectory(), f));
   const directory = getConfigDirectory();
   for (const fileName of files) {
     // NOTE: `dir: string` and `search: boolean` may be added to sniff child directories for configs
     nconf.file(fileName, {
-      file: path.join(directory, fileName)
+      file: path.join(directory, fileName),
     });
   }
 }
 
 function isAppEnvConfigKey(key: string) {
-  return key.slice(0, lowerCaseEnvVariableStart.length).toLowerCase() === lowerCaseEnvVariableStart;
+  return key.slice(
+    0,
+    lowerCaseEnvVariableStart.length,
+  ).toLowerCase() === lowerCaseEnvVariableStart;
 }
 
 function transformAppEnvConfigKey(key: string) {
